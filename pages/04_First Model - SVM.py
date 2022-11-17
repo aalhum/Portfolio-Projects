fac211c5b0174a8f8c2acae5a0f8cd195a02bf7c
nen-code <115601143+aalhum@users.noreply.github.com>
2022-11-16 21:07:55 -0500

    updated SVM model page
    Added decision boundary plot to the svm model where you can select 2 features to plot. Also commented out the rest to simplify.

--- pages/04_First Model - SVM.py
+++ pages/04_First Model - SVM.py
@@ -24,27 +24,27 @@ st.write("A Support Vector Machine (SVM) is an algorithm that is best for linear
 
 #EXAMPLE SVC PLOT
 #this code + the decision boundary plotting code comes from the scikit site
-example_pipe = Pipeline([('example_scaler',StandardScaler()),('example_svc',svm.SVC(kernel='linear'))])
-example_pts, example_labels = make_blobs(n_samples = 100,n_features = 2,centers = 2)
-example_pipe.fit(example_pts,example_labels)
+#example_pipe = Pipeline([('example_scaler',StandardScaler()),('example_svc',svm.SVC(kernel='linear'))])
+#example_pts, example_labels = make_blobs(n_samples = 100,n_features = 2,centers = 2)
+#example_pipe.fit(example_pts,example_labels)
 
 
-fig_ex,ax_ex = plt.subplots()
+#fig_ex,ax_ex = plt.subplots()
 
-plt.scatter(example_pts[:,0],example_pts[:,1],c=example_labels, cmap=plt.cm.Paired)
+#plt.scatter(example_pts[:,0],example_pts[:,1],c=example_labels, cmap=plt.cm.Paired)
 
-DecisionBoundaryDisplay.from_estimator(
-    example_pipe,
-    example_pts,
-    colors="k",
-    levels=[-1,0,1],
-    alpha=0.5,
-    linestyles=["--","-","--"],
-    ax=ax_ex,
-)
+#DecisionBoundaryDisplay.from_estimator(
+#    example_pipe,
+#    example_pts,
+#    colors="k",
+#    levels=[-1,0,1],
+#    alpha=0.5,
+#    linestyles=["--","-","--"],
+#    ax=ax_ex,
+#)
 
-plt.show()
-st.pyplot(fig_ex)
+#plt.show()
+#st.pyplot(fig_ex)
 
 #END OF EXAMPLE
 
@@ -55,67 +55,68 @@ orbits_data = pd.read_csv('orbits.csv')  #read in orbits data
 #st.write(labels.unique())    #check how many unique values are in asteroid classification
 orbits_data = orbits_data.drop(['Object Name','Orbital Reference'],axis=1) #drop columns not used as features
 orbits_data = orbits_data.dropna()  #drop rows with missing values
-cratio = 0.99  #percentage of data used for training
+cratio = 0.05  #percentage of data used for training
 cutoff = round(cratio*orbits_data.shape[0])  #cutoff index that separates training and testing data, 90% training, 10% test
 test_orbits_data = orbits_data.iloc[cutoff::,:] 
 train_orbits_data = orbits_data.iloc[0:cutoff,:]
 test_labels = test_orbits_data.pop('Object Classification')  #get the asteroid classifications/target labels
 train_labels = train_orbits_data.pop('Object Classification')
-subset = ['Orbit Eccentricity','Perihelion Distance (AU)']   #what subset of features to use
-train_subset = train_orbits_data[subset]
-test_subset = test_orbits_data[subset]
 
-#PREPROCESSING:
+allowed_subset = ['Orbit Axis (AU)','Orbit Eccentricity','Orbit Inclination (deg)','Perihelion Argument (deg)','Node Longitude (deg)','Mean Anomoly (deg)','Orbital Period (yr)']  #list of possible features
+
+allowed_train = train_orbits_data[allowed_subset]
+allowed_test = test_orbits_data[allowed_subset]
+
+optimal_features = ['Perihelion Argument (deg)','Mean Anomoly (deg)','Orbital Period (yr)']
+
+allowed_features = train_orbits_data[allowed_subset]
+
+st.write('First, Select 2 of the features below to demonstrate the SVM algorithm below. The Decision Boundary will be plotted as a series of black lines below:')
+
+    
+colum1,colum2 = st.columns(2)
+
+with colum1:
+    svmparameter1 = st.selectbox(label='Choose 1st Feature',options=allowed_features.columns)
+with colum2:
+    svmparameter2 = st.selectbox(label='Choose 2cd Feature',options=allowed_features.columns,index=1)
+
+same_parameter = False
+
+if svmparameter1 == svmparameter2:
+    st.write("You have selected the same feature twice! Please select 2 different features")
+    same_parameter = True
+
+subset = [svmparameter1,svmparameter2]   #what subset of features to use
+
+train_subset = allowed_train[subset]
+test_subset = allowed_test[subset]
+
+#Create Pipeline that includes preprocessing + SVM model
 
 a_pipeline = Pipeline([('ala_scaler',StandardScaler()),('ala_svc',svm.SVC(kernel='poly'))])
 
 
 
 # TRAINING AND TESTING MODEL
-a_pipeline.fit(train_subset,train_labels)  #TRAIN MODEL
+if same_parameter == False:
+    a_pipeline.fit(train_subset,train_labels)  #TRAIN MODEL
 
 
-predictions = a_pipeline.predict(test_subset)   #PREDICT TESTING DATA
-st.write("5-fold cross validation was computed on the training data. The accuracy ratio for each of the 5 folds is given below.")
-accuracies = cross_val_score(a_pipeline,train_subset,train_labels, cv=5)
+#predictions = a_pipeline.predict(test_subset)   #PREDICT TESTING DATA
+#st.write("5-fold cross validation was computed on the training data. The accuracy ratio for each of the 5 folds is given below.")
+#accuracies = cross_val_score(a_pipeline,train_subset,train_labels, cv=5)
 #accuracy = clf.score(test_subset,test_labels)       #OBTAIN ACCURACY
-st.write(accuracies)
+#st.write(accuracies)
 #st.write(np.unique(predictions))
 
 
 
 #st.write(orbits_data['Object Classification'].value_counts())
 
-st.write(confusion_matrix(test_labels,predictions))
+#st.write(confusion_matrix(test_labels,predictions))
 
-#BUTTON DISPLAYS: the training and testing data using containers
-#-----------------------------------------------------------------
-train_container = st.container()
-test_container = st.container()
-traindataexpander1 = st.expander("Open Training Data")
-traindataexpander2 = st.expander("Open Training Labels")
-testdataexpander1 = st.expander("Open Testing Data")
-testdataexpander2 = st.expander("Open Test Labels")
-traincol1, traincol2 = st.columns(2, gap='medium')  
-testcol1, testcol2 = st.columns(2, gap='large')
 
-with train_container:
-    with traincol1:
-        with traindataexpander1:
-            st.dataframe(train_subset)
-    with traincol2:
-        with traindataexpander2:
-            st.dataframe(train_labels)
-        
-with test_container:
-    with testcol1:
-        with testdataexpander1:
-            st.dataframe(test_subset)
-    with testcol2:
-        with testdataexpander2:
-            st.dataframe(test_labels)
-
-#------------------------------------------------------------
 
 #plot decision boundaries
 
@@ -124,16 +125,17 @@ fig,ax = plt.subplots()
 
 sns.scatterplot(data=orbits_data,x=subset[0],y=subset[1],hue = "Object Classification", palette = "muted",ax=ax)
 
-DecisionBoundaryDisplay.from_estimator(
-    a_pipeline,
-    train_subset,
-    plot_method="contour",
-    colors="k",
-    levels = [-1,0,1],
-    alpha=0.5,
-    linestyles = ["--","-", "--"],
-    ax=ax
+if same_parameter == False:
+    DecisionBoundaryDisplay.from_estimator(
+        a_pipeline,
+        train_subset,
+        plot_method="contour",
+        colors="k",
+        levels = [-1,0,1],
+        alpha=0.5,
+        linestyles = ["--","-", "--"],
+        ax=ax
 )
-plt.show()
+
 st.pyplot(fig)
 
