896fc4711f911a5e1d376a81099af9d9caddab18
nen-code <115601143+aalhum@users.noreply.github.com>
2022-12-10 23:22:45 -0500

    updated feature selection
    Updated the feature selection page to use rfecv function with cross validation instead of univariate statistical tests

--- pages/03_Feature_Selection.py
+++ pages/03_Feature_Selection.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import statistics
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -11,6 +12,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.datasets import make_blobs
 from sklearn.feature_selection import RFECV, SelectKBest, chi2
+
+st.title("Feature Selection")
 
 orbits_data = pd.read_csv('orbits.csv')  #read in orbits data
 #st.write(labels.unique())    #check how many unique values are in asteroid classification
@@ -54,28 +57,61 @@ train_labels = train_orbits_data.pop('Object Classification')
 #st.pyplot(fig_opt)
 
 
-#using chi-squared score to score the features (univariate statistical test) and determine which features are more essential for predicting asteroid classification
+#use only an allowed subset of all of the features, create the training set
 allowed_features = train_orbits_data[['Orbit Axis (AU)','Orbit Eccentricity','Orbit Inclination (deg)','Perihelion Argument (deg)','Node Longitude (deg)','Mean Anomoly (deg)','Orbital Period (yr)']]
+new_labels = train_labels.copy()
+quad_labels = new_labels.replace({'Amor Asteroid (Hazard)':'Amor Asteroid','Apollo Asteroid (Hazard)':'Apollo Asteroid','Apohele Asteroid (Hazard)':'Apohele Asteroid','Aten Asteroid (Hazard)':'Aten Asteroid'})
 
-optimal_features = SelectKBest(chi2, k=3).fit(allowed_features,train_labels)
 
-which_features = optimal_features.get_support()
+min_features_to_select = 1
+feature_scaler = StandardScaler()
+#use StandardScaler function to Scale the data to have 0 mean and standard deviation of 1
+standardized_train = feature_scaler.fit_transform(allowed_features)
 
-allowed_expand = st.expander('Show Initial Feature Set (Before Feature Selection)')
-selected_expand = st.expander('Show Selected Features')
+feat_model = svm.SVC(kernel='linear')
+#train the SVM model to use with RFECV
+feat_model.fit(standardized_train,quad_labels)
+#define cross validation fold scheme
+cv = StratifiedKFold(5)  
 
-st.write("Here is the list of features from Orbital_Characteristics, though some were removed for the following reasons:")
-st.write("We removed Object Name and Orbital Reference because they are identifiers")
-st.write("We removed Epoch because this is the starting reference time point and therefore not particularly useful.")
-st.write("We removed Perihelion Distance, Aphelion Distance, Minimum Orbit Intersection Distance, and Asteroid Magnitude because they are used in the definitions of the asteroid classifications and so it did not make sense to use them for prediction.")
+#DEFINE RECURSIVE FEATURE SELECTION WITH CROSS VALIDATION
+ala_rfecv = RFECV(
+    estimator=feat_model,
+    step=1,
+    cv=cv,
+    scoring="accuracy",
+    min_features_to_select=min_features_to_select,
+    n_jobs=2
+)
 
-st.dataframe(allowed_features)
+ala_rfecv.fit(standardized_train,quad_labels)
 
-st.write("Initially we tried performing feature selection by looking at subsets of features and running cross-validation, but this proved to take an inordinate amount of time, even when we reduced the number of samples used drastically.")
-st.write("Therefore we decided to simply use a univariate feature selection (specifically the SelectKBest function in Scikit Learn), where we rank features using a statistical test (ANOVA, Chi-Squared, etc) and then select the top K features.")
-st.write("In our case we arbitrarily chose K=3 for the number of optimal features.")
+st.write("We ran the function RFECV, which does recursive feature elimination using a cross validation scheme to find the optimal number of features") 
+st.write("We used a linear SVM in order to score the features, and we used a 5-fold (stratified) cross validation scheme to find the optimal number of features")
+st.write(f"Optimal number of features: {ala_rfecv.n_features_}")
+st.write(f"out of a total number of {ala_rfecv.n_features_in_} features")
 
-st.write('Below we show which features from the original set were selected.')
-st.write(allowed_features.columns[which_features])   #use the indices to get the names of the selected features
+st.write("Thus including all of the features is the optimal set, which makes sense")
+
+st.write("When the percentage of training data used was increased from 20% to 50% to 75% of the total dataset, the number of optimal features also increased. This is likely because the number of samples increased so the ratios of each output class were different.")
+st.write("The labels for these features were:")
+#st.write()
+
+#feat_pipeline = Pipeline([('ala_feat_scaler',StandardScaler()),('ala_feat_svc',svm.SVC(kernel='linear'))])
+#feat_pipeline.fit(allowed_features,quad_labels)
+#cv = StratifiedKFold(5)
+
+#rfecv = RFECV(
+#    estimator=feat_pipeline,
+#    step=1,
+#    cv=cv,
+#    scoring="accuracy",
+#    min_features_to_select=min_features_to_select,
+#    n_jobs=2,
+#)
+
     
-    
+#rfecv.fit(allowed_features,quad_labels)
+
+#st.write(f"Optimal number of features: {rfecv.n_features_}")
+
